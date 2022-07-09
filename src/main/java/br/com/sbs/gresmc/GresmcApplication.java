0f36e64e002e21422e74deca0ecce2a6ce54c2eb
Sergio Bezerra da Silva <sergio.bezerra@outlook.com>
2022-07-09 14:00:02 -0300

    adicionando primeiros arquivos

---
+++ src/main/java/br/com/sbs/gresmc/GresmcApplication.java
@@ -0,0 +1,13 @@
+package br.com.sbs.gresmc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GresmcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GresmcApplication.class, args);
+	}
+
+}
